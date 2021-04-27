5a684240d838177a458afa5b409d69b45ef4c79a
GP <gregpetropoulos@yahoo.com>
2021-04-27 06:53:26 -0400

    Moved to the Controllers directory under the api folder I have finished the userRoutes.js file

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -1,12 +1,76 @@
 const router = require('express').Router();
-const { User } = require('../../models');
+const { User, Blog, Comment } = require('../../models');
+const withAuth = require('../../utils/auth');
 
+
+
+
+// GET api/users -- Find all users
+
+router.get('/', async (req, res) => {
+  try {
+    const userData = await User.findAll({
+      attributes: {exclude:['password']}
+    })
+    res.status(200).json(userData);
+  }
+  catch (err) {
+    res.status(500).json(err);
+  }
+})
+
+// GET ap/users/id -- Find a user by id
+// with references to users blogs, comments, and comments on a blog title
+router.get('/', async (req, res) => {
+  try {
+    const userData = await User.findOne({
+      attributes: {exclude:['password']},
+      where: {
+        id: req.params.id
+      },
+      include: [
+        {
+          model: Blog,
+          attributes: ['id','title','content','date_created']
+        },
+        {
+          model: Comment,
+          attributes: ['id','comment_text', 'date_created'],
+          include: {
+            model: Blog,
+            attributes: ['title']
+          }
+        }
+      ]
+    })
+    res.status(200).json(userData);
+  }
+  catch (err) {
+    res.status(500).json(err);
+  }
+})
+
+// SIGNUP
 router.post('/', async (req, res) => {
   try {
-    const userData = await User.create(req.body);
+    const userData = await User.create(req.body, {
+      // User inputs info at sign up page
+      username: req.body.username,
+      email: req.body.email,
+      password: req.body.password,
+      github: req.body.github,
+      twitter: req.body.twitter,
+    });
 
     req.session.save(() => {
+      // I think the user_id is generated no user input needed
       req.session.user_id = userData.id;
+      // All user inputs saved
+      req.sessions.username = userData.username;
+      req.sessions.github = userData.github;
+      req.sessions.twitter = userData.twitter;
+      req.sessions.username = userData.username;
+
       req.session.logged_in = true;
 
       res.status(200).json(userData);
@@ -16,9 +80,14 @@ router.post('/', async (req, res) => {
   }
 });
 
+// LOGIN
 router.post('/login', async (req, res) => {
   try {
-    const userData = await User.findOne({ where: { email: req.body.email } });
+    const userData = await User.findOne({ 
+      where: { 
+        email: req.body.email 
+      } 
+    });
 
     if (!userData) {
       res
@@ -37,17 +106,67 @@ router.post('/login', async (req, res) => {
     }
 
     req.session.save(() => {
+      // I think the user_id is generated no user input needed
       req.session.user_id = userData.id;
-      req.session.logged_in = true;
-      
-      res.json({ user: userData, message: 'You are now logged in!' });
-    });
+      // All user inputs saved
+      req.sessions.username = userData.username;
+      req.sessions.github = userData.github;
+      req.sessions.twitter = userData.twitter;
+      req.sessions.username = userData.username;
 
+      req.session.logged_in = true;
+
+      res.status(200).json({userData, message:'You are logged in!'});
+    });
   } catch (err) {
     res.status(400).json(err);
   }
 });
 
+// UPDATE -- /api/users/1
+router.put('/:id', withAuth, async (req, res) => {
+  try{
+    const userData = User.update(req.body,{
+      // Will select records that are about to be updated and
+      //  emit before- + after- Update on each instance
+      individualHooks: true,
+      where:{
+        id: req.params.id
+      }
+    })
+    if(!userData){
+      res.status(404).json({message:'No user found with this id'})
+      return
+    }
+    res.json(userData);
+  }
+  catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+
+// DELETE
+router.put('/:id', withAuth, async (req, res) => {
+  try{
+    const userData = User.destroy(req.body,{
+      where:{
+        id: req.params.id
+      }
+    })
+    if(!userData){
+      res.status(404).json({message:'No user found with this id'})
+      return
+    }
+    res.json(userData);
+  }
+  catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+
+// LOGOUT
 router.post('/logout', (req, res) => {
   if (req.session.logged_in) {
     req.session.destroy(() => {
